8f81ad4d80280ed97764394178c7d7b30ba0cd38
kjr <kjr0814@hotmail.com>
2021-12-04 16:01:49 +0900

    cALL 액션 활용( 전화연결) => 권한으로 앱 종료

--- app/src/main/java/com/neppplus/librarypractice_20211204/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20211204/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.neppplus.librarypractice_20211204
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,6 +15,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupEvents(){
+
+//        전화연결 시 권한 획득
+
+        btnCall.setOnClickListener {
+            val myUri = Uri.parse("tel : 010-6290-1236")
+            val myIntent = Intent(Intent.ACTION_CALL, myUri)
+            startActivity(myIntent)
+
+        }
+
         imgProfile.setOnClickListener {
 
             val myIntent = Intent(this, ViewPhotoActivity::class.java)
