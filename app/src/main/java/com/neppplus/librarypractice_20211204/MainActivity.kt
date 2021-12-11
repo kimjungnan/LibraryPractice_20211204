38ec36d3deaa24acaad971023d202db824e0a30c
kjr <kjr0814@hotmail.com>
2021-12-11 10:51:24 +0900

    인터넷 사진 보여주기

--- app/src/main/java/com/neppplus/librarypractice_20211204/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20211204/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.neppplus.librarypractice_20211204
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
@@ -41,6 +43,8 @@ class MainActivity : AppCompatActivity() {
             override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
                 //  TODO("Not yet implemented")
                 Toast.makeText(applicationContext, "전화거부", Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "전화거부2", Toast.LENGTH_SHORT).show()
+
             }
 
 
@@ -48,7 +52,7 @@ class MainActivity : AppCompatActivity() {
 
         TedPermission.create()
             .setPermissionListener(pl)
-            .setPermissions(Manifest.permission.CALL_PHONE)
+            .setPermissions(Manifest.permission.CALL_PHONE, Manifest.permission.CAMERA)
             .check()
 
 
@@ -60,6 +64,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setValues(){
+
+//        인터넷상에 있는 이미지를  곧바로 이미지뷰에 적용
+
+//        이미지 주소를 변수에 담아주자
+        val imageURL = "https://s3.ap-northeast-2.amazonaws.com/elasticbeanstalk-ap-northeast-2-176213403491/media/magazine_img/magazine_262/%EC%8D%B8%EB%84%A4%EC%9D%BC.jpg"
+
+
+//        glide이용 > imageURL 주소으 ㅣ기름을 반영
+
+        Glide.with(this).load(imageURL).into(imgInter)
 
 
     }
