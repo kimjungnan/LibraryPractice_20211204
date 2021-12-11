61795b343c5082cf8eb9e81b768b304e8681be6b
kjr <kjr0814@hotmail.com>
2021-12-11 11:22:49 +0900

    웹 시작화면 변경

---
+++ app/src/main/java/com/neppplus/librarypractice_20211204/SlashActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.librarypractice_20211204
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SlashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_slash)
+    }
+}
