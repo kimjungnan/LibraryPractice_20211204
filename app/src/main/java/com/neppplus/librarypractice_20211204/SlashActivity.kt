9ec88092e497d8b9d6f66f7c00a4f9ed337e8a11
kjr <kjr0814@hotmail.com>
2021-12-11 11:58:28 +0900

    애니메이션 추가...

--- app/src/main/java/com/neppplus/librarypractice_20211204/SlashActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20211204/SlashActivity.kt
@@ -1,11 +1,31 @@
 package com.neppplus.librarypractice_20211204
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SlashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slash)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents(){
+
+    }
+
+    fun setValues(){
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed( {
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        }, 2000 )
     }
 }
